326eb5e70684e46355516b3b3eed99ef83dc68d8
Benjamin Labas <benjamin.labas@airbus.com>
2017-10-23 16:26:38 +0200

    added RMSE metric

--- ml_lib/utils/metrics.py
+++ ml_lib/utils/metrics.py
@@ -1,5 +1,7 @@
 """Functions for error and loss calculation."""
 
+import math
+
 def mse(y, y_predicted):
     """Calculates MSE of given target and predicted values.
 
@@ -13,6 +15,18 @@ def mse(y, y_predicted):
     m = y.size()
 
     return (1./(2. * m)) * sum((y_predicted - y) ** 2)
+    
+def rmse(y, y_predicted):
+    """Calculates RMSE of given target and predicted values.
+
+    Args:
+        y: Ground truth.
+        y_predicted: Predictions.
+
+    Returns:
+        The RMSE.
+    """
+    return math.sqrt(mse(y, y_predicted))
 
 def rss(y, y_predicted):
     """Calculates RSS (residual sum of squares) for given target
