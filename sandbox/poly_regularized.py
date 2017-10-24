fixed gradient calculation; fixed mse; played around with polynomial reg

sandbox/poly_regularized.py
@@ -2,12 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 from ml_lib.models.linearreg import SgdRegressor
+import ml_lib.prep.scaler as scaler
+import ml_lib.utils.metrics as metrics
 
 def genTestData(instances=100):
     X = 6 * np.random.rand(instances, 1) - 3
-    y = 0.5 * X**2 + X + 2 + np.random.randn(instances, 1)
+
+    X.sort(axis=0)
+
+    y = X ** 3 + 0.5 * X**2 + 0.5 * X + 2 + np.random.randn(instances, 1)
 
     return X, y
 
@@ -18,18 +24,20 @@ def poly(X, deg=200):
 
 if __name__ == '__main__':
 
-    X, y = genTestData()
+    X, y = genTestData(10)
 
-    X_poly = poly(X, deg=10)
+    #X_test = 
 
-    reg = SgdRegressor(epochs=10000, eta0=0.00001)
+    X_poly = poly(X, deg=7)
 
+    print(X_poly.shape)
+
+    reg = LinearRegression(normalize=False)
     reg.fit(X_poly, y)
-
+        
     y_pred = reg.predict(X_poly)
 
-    print(reg.theta)
-
+    plt.figure()
     plt.plot(X, y, 'rx')
-    plt.plot(X, y_pred, 'bo')
+    plt.plot(X, y_pred, 'b-')
     plt.show()