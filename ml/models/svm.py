863587c112ea90e8a605047e3db55a3489efd08f
B Labas <benjamin.labas@gmail.com>
2017-11-08 21:39:50 +0100

    svm skeleton

---
+++ ml/models/svm.py
@@ -0,0 +1,30 @@
+"""Implementation of Support Vector Machine."""
+
+import numpy as np
+
+class SVM(object):
+    """SVM classification model."""
+
+    def __init__(self):
+        """Initializes classifier with hyperparameters."""
+        pass
+
+    def fit(self, X, y):
+        """Fits the model parameters.
+
+        Args:
+            X: Training data set.
+            y: Labels.
+        """
+        pass
+
+    def predict(self, X):
+        """Performs predictions based on fitted model.
+
+        Args:
+            X: Feature set.
+
+        Returns:
+            A numpy array containing the predicted values.
+        """
+        pass
