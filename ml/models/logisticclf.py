6eba3d96644a7d62dd5419bc9260b6d0b50442ce
B Labas <benjamin.labas@gmail.com>
2017-11-08 21:37:11 +0100

    added docstrings for logistic classifier

--- ml/models/logisticclf.py
+++ ml/models/logisticclf.py
@@ -3,6 +3,7 @@
 from logisticreg import LogisticRegression
 
 class LogisticClassifier(LogisticRegression):
+    """Classification model based on Logistic Regression."""
 
     def __init__(
             self,
@@ -12,6 +13,20 @@ class LogisticClassifier(LogisticRegression):
             epochs=100,
             alpha=0.,
             l1_ratio=1.):
+        """Initializes classifier with hyperparameters.
+
+        Regularizes model with elastic net by setting regularization factor
+        alpha. Setting l1_ratio to 1.0 performs l1 regularization (LASSO),
+        l1_ratio to 0.0 for l2 regularization (ridge)
+
+        Args:
+            threshold: Threshold classification is based on.
+            eta0: Starting learning rate.
+            annealing: Rate for annealing the learning rate.
+            epochs: Number of epochs used for training.
+            alpha: Regularization factor
+            l1_ratio: l1 penalty ratio
+        """
         self.threshold = threshold
 
         super(LogisticClassifier, self).__init__(
@@ -22,6 +37,14 @@ class LogisticClassifier(LogisticRegression):
             l1_ratio)
 
     def predict(self, X):
+        """Performs predictions based on fitted model.
+
+        Args:
+            X: Feature set.
+
+        Returns:
+            A numpy array containing the predicted values.
+        """
         result = super(LogisticClassifier, self).predict(X)
 
         return result > self.threshold
