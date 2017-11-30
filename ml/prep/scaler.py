a9d129bdd47bc8914b35ea1b5a615854675a349a
B Labas <benjamin.labas@gmail.com>
2017-11-30 21:45:55 +0100

    added standard scaling

--- ml/prep/scaler.py
+++ ml/prep/scaler.py
@@ -1,5 +1,7 @@
 """Functions for feature scaling."""
 
+import numpy as np
+
 def normalize(X):
     """Normalize features X (min-max-scaling).
 
@@ -11,3 +13,17 @@ def normalize(X):
     """
 
     return (X - X.min()) / (X.max() - X.min())
+
+def standardize(X):
+    """Standardize feature set X.
+
+    Args:
+        X: Feature set to scale.
+
+    Returns:
+        Scaled feature set.
+    """
+    mean = np.mean(X, axis=0)
+    variance = np.var(X, axis=0)
+
+    return (X - mean) / variance
