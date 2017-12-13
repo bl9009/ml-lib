14cbf0544dcbc52d3b166113f49f9f1b6d8f823d
B Labas <benjamin.labas@gmail.com>
2017-12-13 19:58:28 +0100

    added k-means model stub

---
+++ ml/models/kmeans.py
@@ -0,0 +1,29 @@
+"""K-Means clustering model."""
+
+import numpy as np
+
+class KMeans(object):
+    """K-Means clustering model implementation based on Lloyd's algorithm."""
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        """Fits the model using Lloyd's algorithm.
+
+        Args:
+            X: Training data set.
+            y: Labels.
+        """
+        pass
+
+    def predict(self, X):
+        """Performs predictions based on fitted model.
+
+        Args:
+            X: Feature set.
+
+        Returns:
+            A numpy array containing the predicted values.
+        """
+        pass
